4b33e5c66bb4f05d0a011f2e696570776f2a5f9f
stacksonstack <staceycarrillo44@gmail.com>
2020-10-20 15:16:53 -0400

    finished new user form/ pokemon & user show/index pages

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,2 +1,3 @@
 class PartiesController < ApplicationController
+    
 end
--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,19 @@
 class PokemonsController < ApplicationController
+
+    def index
+        @pokemons = Pokemon.all
+    end
+
+    def show
+        @pokemon = Pokemon.find(params[:id])
+    end
+    
+
+    private
+
+    def pokemon_params
+        params.require(:pokemon).permit(:name, :pokemontype, :description, :front_img, :back_img, :pokedex_number)
+    end
+
+
 end
